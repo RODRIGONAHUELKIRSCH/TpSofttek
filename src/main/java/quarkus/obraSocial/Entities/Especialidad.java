70fc5051cbdfd886c8590f04b396181d6d4f768f
RODRIGONAHUELKIRSCH <mrnaaaa@gmail.com>
2024-06-17 03:46:27 -0300

    swaggermain

--- src/main/java/quarkus/obraSocial/Entities/Especialidad.java
+++ src/main/java/quarkus/obraSocial/Entities/Especialidad.java
@@ -1,18 +1,21 @@
 package quarkus.obraSocial.Entities;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Especialidad")
+@ApiModel(value="Especialidad",description="Creacion tabla especialidad")
 public class Especialidad extends PanacheEntity{
 
-	
+	@ApiModelProperty(notes="nombre de la especialidad del medico")
 	@Column(name="area_especialidad",nullable=false)
 	private String area_especialidad;
-
+	
 	public String getArea_especialidad() {
 		return area_especialidad;
 	}
--- src/main/java/quarkus/obraSocial/Entities/Receta.java
+++ src/main/java/quarkus/obraSocial/Entities/Receta.java
@@ -1,6 +1,8 @@
 package quarkus.obraSocial.Entities;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,12 +12,15 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="Receta")
+@ApiModel(value="Receta",description="creacion de la tabla receta")
 public class Receta extends PanacheEntity{
 
+	@ApiModelProperty(notes="Turno para el cual se emite la receta")
     @OneToOne(cascade=CascadeType.ALL)
 	@JoinColumn(name="id_turno",referencedColumnName="id")
 	private Turno turno;
     
+	@ApiModelProperty(notes="fecha hora de emision receta")
     @Column(name="fecha_hora")
     private String fecha_hora;
 
